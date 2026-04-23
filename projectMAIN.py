1da52f1f8497110f8e5d99e06646a28c82f8af30
EladHaHa <eladhahn3@gmail.com>
2026-04-23 09:30:57 +0300

    Update projectMAIN.py

--- projectMAIN.py
+++ projectMAIN.py
@@ -3,7 +3,12 @@ import streamlit as st
 tab1, tab2, tab3 = st.tabs(["Log In", "Home", "About"])
 
 with tab1:
-  st.write("היי")
+  username = st.text_input("Username")
+  password = st.text_input("Password", type="password")
+
+  if(st.button("Log In"):
+    st.success(f"{username} is logged in"}
+  
 with tab2:
   st.write("היי")
 with tab3:
