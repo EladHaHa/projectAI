c4ac0393f2103c44461862982511a889093a5dcc
EladHaHa <eladhahn3@gmail.com>
2026-09-09 10:30:00 +0300

    Update projectMAIN.py

--- projectMAIN.py
+++ projectMAIN.py
@@ -59,12 +59,46 @@ st.title("My Prediction App")
 # 5 INPUTS
 # =========================
 
-input1 = st.number_input("Input 1", value=0.0)
-input2 = st.number_input("Input 2", value=0.0)
-input3 = st.number_input("Input 3", value=0.0)
-input4 = st.number_input("Input 4", value=0.0)
-input5 = st.number_input("Input 5", value=0.0)
+gender = st.input("Gender", value="male/female")
+race = st.input("Race", value="A/B/C/D/E)
+parents = st.input("Parent's Education", value="high school/some high shool/some college/associate's degree/bachelor's degree/master's degree")
+lunch = st.input("Lunch", value="Free/Reduced/Complete")
+prep = st.input("Preperation for test", value="None/Full")
 
+if gender == "male":
+    input1 = 0
+else if gender == "female":
+   input1=1
+if race=='A':
+    input2=0
+if race=='B':
+    input2=1
+if race=='C':
+    input2=2
+if race=='D':
+    input2=3
+if race=='E':
+    input2=4
+if parents == "high school":
+    input3=1
+if parents == "some college":
+    input3=0
+if parents == "associate's degree":
+    input3=2
+if parents == "some high school":
+    input3=3
+if parents == "bachelor's degree":
+    input3=4
+if parents == "master's degree":
+    input3=5
+if lunch=="Free" or lunch=="Reduced":
+    input4=1
+if lunch=="Complete":
+    input4=0
+if prep=="None":
+    input5=0
+if prep=="Full":
+    input5=1
 # =========================
 # BUTTON
 # =========================
@@ -72,9 +106,9 @@ input5 = st.number_input("Input 5", value=0.0)
 if st.button("Predict"):
 
     # Put your model/calculation here
-    output1 = input1 + input2
-    output2 = input3 + input4
-    output3 = input5
+    output1 = model.predict([input1,input2,input3,input4,input5])[0,0]
+    output2 = model.predict([input1,input2,input3,input4,input5])[0,1]
+    output3 = model.predict([input1,input2,input3,input4,input5])[0,2]
 
     # =========================
     # 3 OUTPUTS
@@ -82,11 +116,11 @@ if st.button("Predict"):
 
     st.subheader("Results")
 
-    st.write("Output 1:")
+    st.write("Math grade:")
     st.write(output1)
 
-    st.write("Output 2:")
+    st.write("Reading grade:")
     st.write(output2)
 
-    st.write("Output 3:")
+    st.write("Writing grade:")
     st.write(output3)
